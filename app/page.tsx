4151189e800e0a08e5fd84d0700b3c28996ce3d8
JiyeSun <jiyesun217@gmail.com>
2026-03-20 14:05:49 -0400

    Make span text bold in header

--- app/page.tsx
+++ app/page.tsx
@@ -137,7 +137,7 @@ export default function Home() {
           <h1 className="text-4xl font-bold mb-8">
             Pattern Reasoning Challenge
             <br />
-            <span className="text-cyan-400 text-xl font-xl">
+            <span className="text-cyan-400 text-xl font-xl font-bold">
               Solve problems with AI support
             </span>
           </h1>
